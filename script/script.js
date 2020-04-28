c98ffe96236dabbdba3ad2a8f5f0c1df48c54b5a
unknown <edu.dovale@gmail.com>
2020-04-28 20:41:36 -0300

    Projeto finalizado

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
+
+// Função validaEmail que trata todos os possíveis erros na digitação do e-mail: falta de '@', espaço, '.' ... 
+
 function validaEmail(field) {
     usuario = field.value.substring(0, field.value.indexOf("@"));
     dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
 
-    emaildigitado = document.getElementById('idemail');
+    emaildigitado = document.getElementById('idemail'); // captura o que foi digitado no campo email
 
     if ((usuario.length >= 1) &&
         (dominio.length >= 3) &&
@@ -15,11 +18,21 @@ function validaEmail(field) {
         (dominio.lastIndexOf(".") < dominio.length - 1)) {
 
         alert("O email " + emaildigitado.value + " foi enviado");
-        document.getElementById('divmsg').innerHTML = "Bem vindo " + emaildigitado.value + "!";
         
+        //exibe a mensagem de boas vindas
+        document.getElementById('exibemsg').innerHTML = "Bem vindo " + emaildigitado.value + "!"; 
+
+        //apaga na tela o e-mail digitado
+        document.getElementById('idemail').value = "";
+
     }
     else {
         alert("E-mail invalido");
+
+        //apaga a mensagem de boas vindas na tela, caso já tenha sido digiitado algum e-mail válido.
+        document.getElementById('exibemsg').innerHTML = " ";
+
     };
 }
+
 
